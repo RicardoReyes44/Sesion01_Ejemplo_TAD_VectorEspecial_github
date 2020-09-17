16c9ec3ec200edbd5ede8a3361befc5894d467a8
RicardoReyes44 <rickyboy1002@gmail.com>
2020-09-17 17:16:59 -0500

    S agregaron los metodos de aumentar y disminuir tamaño a la clase 'VectorEspecial'

--- src/PruebaVectorEspecial.java
+++ src/PruebaVectorEspecial.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -10,8 +11,8 @@ import java.util.Scanner;
  * 6) Mostrar todos los elementos.............  [HECHO]
  * 7) Mostrar elemento del inicio.............  [HECHO]
  * 8) Mostrar elemento del fin.............  [HECHO]
- * 9) Aumentar tamaño del arreglo
- * 10) Dismnuir tamaño del arreglo
+ * 9) Aumentar tamaño del arreglo.............  [HECHO]
+ * 10) Dismnuir tamaño del arreglo.............  [HECHO]
  * 11) Insertar elemento en posicion especifica
  * 12) Eliminar elemento de posicion especifica
  * 13) Invertir el vector
@@ -70,6 +71,40 @@ class VectorEspecial{
 		System.out.println(edades[this.tamaño-1]);
 	}
 	
+	// 9
+    public void aumentarTamaño() throws IOException{
+		System.out.println("Introduce el tamaño extra: ");
+		byte aumento = entrada.nextByte();
+    	
+		if(aumento>0) {
+			int edadesTemp[] = edades.clone();
+		    edades = new int[edades.length+aumento];
+		    
+		    for(int i=0; i<edadesTemp.length; i++) {
+			    edades[i] = edadesTemp[i];
+		    }
+		}else {
+			throw new IOException("Ya se acabo el juego, no se poner ninguna marca");
+		}
+		
+	}
+	
+    // 10
+	public void disminuirTamaño(int decremento) {
+		
+		if(decremento>0) {
+			int edadesTemp[] = edades.clone();
+			edades = new int[edades.length-decremento];
+			
+			for(int i=0; i<edades.length; i++) {
+				edades[i] = edadesTemp[i];
+			}
+		}else if(decremento==edades.length) {
+			edades = new int[0];
+		}
+		
+	}
+	
 }
 
 
@@ -78,13 +113,14 @@ public class PruebaVectorEspecial {
 	public static void main(String[] args) {
 		
 		Scanner entrada = new Scanner(System.in);
+		VectorEspecial ve = new VectorEspecial();
 		byte opcion;
 		boolean candado = false;
 		
 		do {
 			System.out.print("Introduce una opcion:\n"
-			+ "1) Crear (instanciacion)\n"
-			+ "2) Llenar (inicializacion)\n"
+			+ "1) Crear nuevo array\n"
+			+ "2) Llenar array\n"
 			+ "3) obtener inicio\n"
 		    + "4) obtener fin\n"
 			+ "5) obtener cantidad de elementos\n"
@@ -130,6 +166,8 @@ public class PruebaVectorEspecial {
 			case 14:
 				candado=true;
 				break;
+			default:
+				System.out.println("\nNo existe esa opcion, por favor vuelve a intentarlo");
 			}
 		
 		}while(candado!=true);
